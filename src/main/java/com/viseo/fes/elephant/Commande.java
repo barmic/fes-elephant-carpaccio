510ea5782342acaa2a31fa3f662797a34228f210
Michel Barret <michel.barret@gmail.com>
2017-02-23 21:18:20 +0100

    inférieur ou égale

--- src/main/java/com/viseo/fes/elephant/Commande.java
+++ src/main/java/com/viseo/fes/elephant/Commande.java
@@ -27,15 +27,15 @@ public class Commande {
     }
 
     public int reduction() {
-        if (prixCommand > 50_000) {
+        if (prixCommand >= 50_000) {
             return 15;
-        } if (prixCommand > 10_000) {
+        } if (prixCommand >= 10_000) {
             return 10;
-        } if (prixCommand > 7_000) {
+        } if (prixCommand >= 7_000) {
             return 7;
-        } if (prixCommand > 5_000) {
+        } if (prixCommand >= 5_000) {
             return 5;
-        } if (prixCommand > 1_000) {
+        } if (prixCommand >= 1_000) {
             return 3;
         }
         return 0;
